9419a98fee5456fff4f4289c3366e0255e598164
elvislau74 <126195092+elvislau74@users.noreply.github.com>
2023-06-14 20:10:52 -0400

    debug product-routes.js file's delete route, added response for success

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -148,6 +148,7 @@ router.delete('/:id', async (req, res) => {
       res.status(404).json({message: 'No product found with that id!'});
       return;
     }
+    res.status(200).json(productData);
 
   } catch (err) {
     console.log(err);
